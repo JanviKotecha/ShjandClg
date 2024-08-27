54381baf1a8e376618b6c6b77c524fea49d70803
sangnai908 <dsangani44@gmail.com>
2024-08-27 16:52:06 +0530

    Step2 add required * in form

--- Step2.php
+++ Step2.php
@@ -1,30 +1,30 @@
-<div class="multisteps-form__panel shadow p-4 rounded bg-white " data-animation="scaleIn">
+<div class="multisteps-form__panel shadow p-4 rounded bg-white" data-animation="scaleIn">
     <h3 class="multisteps-form__title" style="padding-left:18px;">Present Address Details
     </h3>
     <div class="multisteps-form__content">
         <div class="form-row mt-4">
-            <div class="col-12 mt-4 px-1">
-                <p>Address</p>
+            <div class="col-12 mt-2 px-1">
+                <p>Address <sup style="color:#9194A0">*</sup></p>
                 <textarea class="multisteps-form__textarea form-control" name="present_address"
                     id="present_address"></textarea>
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>State</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>State <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="present_state"
                     id="present_state" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>District</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>District <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="present_district"
                     id="present_district" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>Taluka</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>Taluka <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="present_taluka"
                     id="present_taluka" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>Pincode</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>Pincode <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="number" name="present_pincode"
                     id="present_pincode" />
             </div>
@@ -33,7 +33,7 @@
         <h3 class="multisteps-form__title mt-4" style="padding-left:18px;">Permanent Address
             Details</h3>
         <div class="form-row mt-4">
-            <div class="col-12 mt-4 px-1">
+            <div class="col-12 mt-2 px-1">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" name="same_as_present" id="same_as_present">
                     <label class="form-check-label" for="same_as_present">
@@ -41,28 +41,28 @@
                     </label>
                 </div>
             </div>
-            <div class="col-12 mt-4 px-1">
-                <p>Address</p>
+            <div class="col-12 mt-2 px-1">
+                <p>Address <sup style="color:#9194A0">*</sup></p>
                 <textarea class="multisteps-form__textarea form-control" name="permanent_address"
                     id="permanent_address"></textarea>
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>State</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>State <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="permanent_state"
                     id="permanent_state" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>District</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>District <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="permanent_district"
                     id="permanent_district" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-1">
-                <p>Taluka</p>
+            <div class="col-12 col-sm-6 mt-2 px-1">
+                <p>Taluka <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="text" name="permanent_taluka"
                     id="permanent_taluka" />
             </div>
-            <div class="col-12 col-sm-6 mt-4 px-10">
-                <p>Pincode</p>
+            <div class="col-12 col-sm-6 mt-2 px-10">
+                <p>Pincode <sup style="color:#9194A0">*</sup></p>
                 <input class="multisteps-form__input form-control" type="number" name="permanent_pincode"
                     id="permanent_pincode" />
             </div>
