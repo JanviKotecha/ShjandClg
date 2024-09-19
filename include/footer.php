591da4cf5c1f11dbb54cdf6073aee1ea5531d2db
sangnai908 <dsangani44@gmail.com>
2024-09-19 11:26:54 +0530

    footer done

--- include/footer.php
+++ include/footer.php
@@ -1,107 +1,93 @@
 <footer class="footer ">
     <div class="container">
-      <div class="footer__wrapper">
-        <div class="footer__top footer__top--style1">
-          <div class="row gy-5 gx-4">
-            <div class="col-md-6">
-              <div class="footer__about">
-                <a href="index.php" class="footer__about-logo"><img src="assets/images/logo/logo-dark.png"
-                    alt="Logo"></a>
-                <p class="footer__about-moto ">Welcome to our trading site! We offer the best, most
-                  affordable products and services around. Shop now and start finding great deals!</p>
-              
-              </div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="footer__links">
-                <div class="footer__links-tittle">
-                  <h6>Quick links</h6>
-                </div>
-                <div class="footer__links-content">
-                  <ul class="footer__linklist">
-                    <li class="footer__linklist-item"> <a href="about.html">About Us</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="team.html">Teams</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="service.html">Services</a> </li>
-                    <li class="footer__linklist-item"> <a href="#">Features</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+        <div class="footer__wrapper">
+            <div class="footer__top footer__top--style1 pb-0">
+                <div class="row gy-5 gx-4">
+                    <div class="col-md-6">
+                        <div class="footer__about">
+                            <a href="index.php" class="footer__about-logo"><img src="assets/images/logo/logo-dark.png"
+                                    alt="Logo"></a>
+                            <p class="footer__about-moto ">Empowering students with knowledge and values since 2013.
+                                Dedicated to excellence in education and holistic development.
+                                Sahajand College - Shaping futures with a commitment to quality.</p>
 
-            </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="footer__links">
-                <div class="footer__links-tittle">
-                  <h6>Support</h6>
-                </div>
-                <div class="footer__links-content">
-                  <ul class="footer__linklist">
-                    <li class="footer__linklist-item"> <a href="#">Terms & Conditions</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="#">Privacy Policy</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="#">FAQs</a></li>
-                    <li class="footer__linklist-item"> <a href="#">Support Center</a> </li>
-                  </ul>
-                </div>
-              </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-6">
+                        <div class="footer__links">
+                            <div class="footer__links-tittle">
+                                <h6>Quick links</h6>
+                            </div>
+                            <div class="footer__links-content">
+                                <ul class="footer__linklist">
+                                    <li class="footer__linklist-item"> <a href="our-identity.php">Who we are</a> </li>
+                                    <li class="footer__linklist-item"> <a href="why-study-at-shjanand-college.php">Why Study at Sahajanand College</a> </li>
+                                    <li class="footer__linklist-item"> <a href="courses.php">Courses</a>
+                                    </li>
+                                    <li class="footer__linklist-item"> <a href="events.php">Events</a>
+                                    </li>
+                                    <li class="footer__linklist-item"> <a href="alumni.php">Alumni</a> </li>
+                                    <li class="footer__linklist-item"> <a href="contact.php">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
 
-            </div>
-            <div class="col-md-2 col-sm-4">
-              <div class="footer__links">
-                <div class="footer__links-tittle">
-                  <h6>Company</h6>
-                </div>
-                <div class="footer__links-content">
-                  <ul class="footer__linklist">
-                    <li class="footer__linklist-item"> <a href="#">Careers</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="#">Updates</a>
-                    </li>
-                    <li class="footer__linklist-item"> <a href="#">Job</a> </li>
-                    <li class="footer__linklist-item"> <a href="#">Announce</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-6">
+                        <div class="footer__links">
+                            <div class="footer__links-tittle">
+                                <h6>Admission</h6>
+                            </div>
+                            <div class="footer__links-content">
+                                <ul class="footer__linklist">            
+                                    <li class="footer__linklist-item"> <a href="how-to-apply.php">How to Apply</a> </li>
+                                    <li class="footer__linklist-item"> <a href="admission-offices.php">Admission Offices</a></li>
+                                    <li class="footer__linklist-item"> <a href="admissions.php">Admission </a></li>
+                                </ul>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="footer__bottom">
+                <div class="footer__end">
+                    <div class="footer__end-copyright">
+                        <p class=" mb-0">© 2024 All Rights Reserved By <a href="#" target="_blank">Sahajanand
+                                College</a> </p>
+                    </div>
+                    <div>
+                        <ul class="social">
+                            <li class="social__item">
+                                <a href="#" class="social__link social__link--style22"><i
+                                        class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li class="social__item">
+                                <a href="#" class="social__link social__link--style22 "><i
+                                        class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="social__item">
+                                <a href="#" class="social__link social__link--style22"><i
+                                        class="fa-brands fa-linkedin-in"></i></a>
+                            </li>
+                            <li class="social__item">
+                                <a href="#" class="social__link social__link--style22"><i
+                                        class="fab fa-youtube"></i></a>
+                            </li>
+                            <li class="social__item">
+                                <a href="#" class="social__link social__link--style22 "><i
+                                        class="fab fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="footer__bottom">
-          <div class="footer__end">
-            <div class="footer__end-copyright">
-              <p class=" mb-0">© 2024 All Rights Reserved By <a href="#"
-                  target="_blank">Sahajanand college</a> </p>
-            </div>
-            <div>
-              <ul class="social">
-                <li class="social__item">
-                  <a href="#" class="social__link social__link--style22"><i class="fab fa-facebook-f"></i></a>
-                </li>
-                <li class="social__item">
-                  <a href="#" class="social__link social__link--style22 "><i class="fab fa-instagram"></i></a>
-                </li>
-                <li class="social__item">
-                  <a href="#" class="social__link social__link--style22"><i class="fa-brands fa-linkedin-in"></i></a>
-                </li>
-                <li class="social__item">
-                  <a href="#" class="social__link social__link--style22"><i class="fab fa-youtube"></i></a>
-                </li>
-                <li class="social__item">
-                  <a href="#" class="social__link social__link--style22 "><i class="fab fa-twitter"></i></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
     <div class="footer__shape">
-      <span class="footer__shape-item footer__shape-item--1"><img src="assets/images/footer/1.png"
-          alt="shape icon"></span>
-      <span class="footer__shape-item footer__shape-item--2"> <span></span> </span>
+        <span class="footer__shape-item footer__shape-item--1"><img src="assets/images/footer/1.png"
+                alt="shape icon"></span>
+        <span class="footer__shape-item footer__shape-item--2"> <span></span> </span>
     </div>
-  </footer>
+</footer>
