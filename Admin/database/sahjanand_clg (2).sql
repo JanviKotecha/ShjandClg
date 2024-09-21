34786acdc4b61e26ba80688533c35112e887362a
sangnai908 <dsangani44@gmail.com>
2024-09-21 22:12:02 +0530

    database update

--- Admin/database/sahjanand_clg (2).sql
+++ Admin/database/sahjanand_clg (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2024 at 07:41 AM
+-- Generation Time: Sep 21, 2024 at 06:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -128,14 +128,7 @@ CREATE TABLE `alumni` (
 --
 
 INSERT INTO `alumni` (`id`, `img`, `name`, `jobPosition`, `c_name`, `c_logo`, `created_at`, `updated_at`) VALUES
-(1, '', 'sdcf', 'fscdfszdf', 'fzdsfs', '', '2024-09-01 12:59:34', '2024-09-01 14:59:20'),
-(4, '1725529626.png', 'Courtney Pope1', 'Eveniet sunt est 1', 'Cairo Hensley1', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(5, '1725529626.png', 'Sangani D', 'Full stack', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(6, '1725529626.png', 'ds', 'Full stack d', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(7, '1725529626.png', 'sd', 'Full stack d', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(8, '1725529626.png', 'DS', 'Full stack dev', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(9, '1725529626.png', 'SD', 'Full stack dev', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06'),
-(10, '1725529626.png', 'SDSDSDSD', 'Full stack dev', 'epsilon', '1725529626.png', '2024-09-05 09:47:06', '2024-09-05 15:17:06');
+(4, '1726936647.png', 'Courtney Pope1', 'Eveniet sunt est 1', 'Cairo Hensley1', '1726936647.png', '2024-09-21 16:37:27', '2024-09-21 22:07:27');
 
 -- --------------------------------------------------------
 
@@ -336,171 +329,6 @@ INSERT INTO `login` (`id`, `username`, `email`, `phone`, `password`, `dt`, `upda
 -- --------------------------------------------------------
 
 --
--- Table structure for table `notificationhistory`
---
-
-CREATE TABLE `notificationhistory` (
-  `id` int(5) NOT NULL,
-  `date` varchar(100) NOT NULL,
-  `msg` varchar(100) NOT NULL,
-  `time` varchar(50) NOT NULL,
-  `type` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `notificationhistory`
---
-
-INSERT INTO `notificationhistory` (`id`, `date`, `msg`, `time`, `type`) VALUES
-(39, '04/04/2024', 'test Enroll Ratione maxime cupid Course', '03:16 pm', 'course'),
-(42, '04/04/2024', ' Purchase New Membership - 20-03-2024 To 30-04-2024', '03:40 pm', 'member_user'),
-(43, '04/04/2024', 'test Enroll Ratione maxime cupid Course', '03:45 pm', 'course'),
-(45, '04/04/2024', 'test Enroll React Js for Begginer Course', '03:53 pm', 'course'),
-(46, '05/04/2024', 'test Enroll Html Basic Course', '10:49 am', 'course'),
-(47, '05/04/2024', 'test Enroll Html Basic Course', '10:49 am', 'course'),
-(48, '05/04/2024', 'test Enroll Angular Basic Course', '10:49 am', 'course'),
-(49, '05/04/2024', 'New Review Add by :- dhruvi', '06:59 pm', 'review'),
-(50, '05/04/2024', 'New Review Add by :- Sangani Dhruvi', '07:09 pm', 'review'),
-(51, '08/04/2024', 'Dhruvi Sangani Enroll Angular Basic Course', '01:02 pm', 'course'),
-(52, '08/04/2024', 'Dhruvi Sangani Enroll Angular Basic Course', '01:02 pm', 'course'),
-(53, '09/04/2024', 'Dhruvi Sangani Enroll Html Basic Course', '06:54 pm', 'course'),
-(54, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '03:49 pm', 'course'),
-(55, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '03:57 pm', 'course'),
-(56, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:18 pm', 'course'),
-(57, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:20 pm', 'course'),
-(58, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:21 pm', 'course'),
-(59, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:23 pm', 'course'),
-(60, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:26 pm', 'course'),
-(61, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:28 pm', 'course'),
-(62, '10/04/2024', 'Dhruvi Sangani Enroll Angular Basic Course', '04:34 pm', 'course'),
-(63, '10/04/2024', 'Dhruvi Sangani Enroll Angular Basic Course', '04:44 pm', 'course'),
-(64, '10/04/2024', 'Dhruvi Sangani Enroll Html Basic Begginer Course', '04:47 pm', 'course'),
-(65, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '02:19 pm', 'member_user'),
-(66, '15/04/2024', 'Dhruvi Sangani Enroll React Native for Begginer Course', '02:30 pm', 'course'),
-(67, '15/04/2024', 'Dhruvi Sangani Enroll React Js for Begginer Course', '02:32 pm', 'course'),
-(68, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '04:00 pm', 'member_user'),
-(69, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '04:09 pm', 'member_user'),
-(70, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '04:13 pm', 'member_user'),
-(71, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '05:48 pm', 'member_user'),
-(72, '15/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '05:52 pm', 'member_user'),
-(73, '16/04/2024', 'Dhruvi Sangani Purchase New Membership - 20-03-2024 To 30-04-2024', '09:46 am', 'member_user'),
-(74, '16/04/2024', 'Dhruvi Sangani Renew Membership - 20-03-2024 To 30-04-2024', '09:58 am', 'member_user'),
-(75, '20/04/2024', 'Dhruvi Sangani Enroll React Native for Begginer Course', '03:33 pm', 'course'),
-(76, '20/04/2024', 'Dhruvi Sangani Enroll Angular Basic Course', '03:34 pm', 'course'),
-(77, '20/04/2024', 'New User Register :- test', '03:51 pm', 'user'),
-(78, '20/04/2024', 'New User Register :- Testing', '03:59 pm', 'user'),
-(79, '20/04/2024', 'New User Register :- testing', '05:54 pm', 'user'),
-(80, '22/04/2024', 'New User Register :- sadas', '05:12 pm', 'user'),
-(81, '23/04/2024', 'New User Register :- dhruvi', '11:11 am', 'user'),
-(82, '23/04/2024', 'dhruvi Purchase New Membership - 20-03-2024 To 30-04-2024', '11:28 am', 'member_user'),
-(83, '23/04/2024', 'dhruvi Enroll React Native for Begginer Course', '11:40 am', 'course'),
-(84, '29/04/2024', 'New User Register :- Dhruvi Sangani', '11:24 am', 'user'),
-(85, '29/04/2024', 'New User Register :- DhruviSangani', '04:42 pm', 'user'),
-(86, '29/04/2024', 'New User Register :- sycyte', '04:46 pm', 'user'),
-(87, '29/04/2024', 'New User Register :- sycyte', '04:47 pm', 'user'),
-(88, '29/04/2024', 'New User Register :- toqygalim', '04:55 pm', 'user'),
-(89, '29/04/2024', 'New User Register :- wikyzy', '04:58 pm', 'user'),
-(90, '29/04/2024', 'New User Register :- bysebaw', '05:10 pm', 'user'),
-(91, '29/04/2024', 'New User Register :- wujod', '05:14 pm', 'user'),
-(92, '29/04/2024', 'New User Register :- xuponetydi', '05:16 pm', 'user'),
-(93, '30/04/2024', 'New User Register :- test', '10:49 am', 'user'),
-(94, '30/04/2024', 'New User Register :- test new', '10:51 am', 'user'),
-(95, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '11:08 am', 'member_user'),
-(96, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '11:13 am', 'member_user'),
-(97, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '11:13 am', 'member_user'),
-(98, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '11:14 am', 'member_user'),
-(99, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '04:56 pm', 'member_user'),
-(100, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:09 pm', 'member_user'),
-(101, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:25 pm', 'member_user'),
-(102, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:27 pm', 'member_user'),
-(103, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:28 pm', 'member_user'),
-(104, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:29 pm', 'member_user'),
-(105, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:29 pm', 'member_user'),
-(106, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:55 pm', 'member_user'),
-(107, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:05 pm', 'member_user'),
-(108, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:06 pm', 'member_user'),
-(109, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:08 pm', 'member_user'),
-(110, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:16 pm', 'member_user'),
-(111, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:17 pm', 'member_user'),
-(112, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:22 pm', 'member_user'),
-(113, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:22 pm', 'member_user'),
-(114, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:25 pm', 'member_user'),
-(115, '02/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '06:27 pm', 'member_user'),
-(116, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '09:30 am', 'member_user'),
-(117, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '09:32 am', 'member_user'),
-(118, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '09:48 am', 'member_user'),
-(119, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '09:50 am', 'member_user'),
-(120, '03/05/2024', 'Dhruvi Sangani Renew Membership - 01-03-2024 To 31-07-2024', '10:03 am', 'member_user'),
-(121, '03/05/2024', 'Dhruvi Sangani Renew Membership - 01-03-2024 To 31-07-2024', '10:05 am', 'member_user'),
-(122, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '10:41 am', 'member_user'),
-(123, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '11:33 am', 'member_user'),
-(124, '03/05/2024', 'Dhruvi Sangani Purchase New Membership - 01-03-2024 To 31-07-2024', '05:58 pm', 'member_user'),
-(125, '13/05/2024', 'New User Register :- Dhruvi Sangani', '11:02 am', 'user');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `notification_count`
---
-
-CREATE TABLE `notification_count` (
-  `user` int(11) NOT NULL,
-  `member_user` int(11) NOT NULL,
-  `review` int(11) NOT NULL,
-  `course` int(11) NOT NULL,
-  `blog` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `notification_count`
---
-
-INSERT INTO `notification_count` (`user`, `member_user`, `review`, `course`, `blog`) VALUES
-(5, 0, 7, 10, 7);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `privacy`
---
-
-CREATE TABLE `privacy` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `privacy`
---
-
-INSERT INTO `privacy` (`id`, `title`, `description`, `updated_at`) VALUES
-(1, 'Privacy Policy', '<p>1. Each Member agrees to abide by the &ldquo;Terms and Conditions &ldquo;of FIP membership, as set forth herein. FIP may change the Terms and Conditions at any time with or without notice.</p>\r\n\r\n<p>2. The membership will grant you certain rights and priorities in the form of participation in future events and activities, Concessions, Reservations, or fee waivers to our majority paid courses.</p>\r\n\r\n<p>3. As long as you remain a member in good standing, you may use FIP&#39;s name and logo, in the format and with the notices provided or requested by FIP, solely to indicate your membership in FIP.</p>\r\n\r\n<p>4. Please note that this membership will NOT give any member a right to take part in the decision-making process at the FIP.</p>\r\n\r\n<p>5. This membership fee is annual and for the current Financial Year 2023-24 which is repayable annually as and when due.</p>\r\n\r\n<p>6. FIP reserves the right of allocating membership upon submission of a request from such person in prescribed form and manner. The acceptance of such form shall be determined in the sole and absolute discretion of the FIP.</p>\r\n\r\n<p>You agree to share information entered on this page with Federation of Indian Professionals (owner of this page) and Razorpay, adhering to applicable laws.</p>\r\n', '2024-03-26 14:55:26');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `terms`
---
-
-CREATE TABLE `terms` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `terms`
---
-
-INSERT INTO `terms` (`id`, `title`, `description`, `updated_at`) VALUES
-(1, 'Terms & Condition', '<p>1. Each Member agrees to abide by the &ldquo;Terms and Conditions &ldquo;of FIP membership, as set forth herein. FIP may change the Terms and Conditions at any time with or without notice.</p>\r\n\r\n<p>2. The membership will grant you certain rights and priorities in the form of participation in future events and activities, Concessions, Reservations, or fee waivers to our majority paid courses.</p>\r\n\r\n<p>3. As long as you remain a member in good standing, you may use FIP&#39;s name and logo, in the format and with the notices provided or requested by FIP, solely to indicate your membership in FIP.</p>\r\n\r\n<p>4. Please note that this membership will NOT give any member a right to take part in the decision-making process at the FIP.</p>\r\n\r\n<p>5. This membership fee is annual and for the current Financial Year 2023-24 which is repayable annually as and when due.</p>\r\n\r\n<p>6. FIP reserves the right of allocating membership upon submission of a request from such person in prescribed form and manner. The acceptance of such form shall be determined in the sole and absolute discretion of the FIP.</p>\r\n\r\n<p>You agree to share information entered on this page with Federation of Indian Professionals (owner of this page) and Razorpay, adhering to applicable laws.</p>\r\n', '2024-03-26 14:54:31');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `testimonial`
 --
 
@@ -543,51 +371,8 @@ CREATE TABLE `topers` (
 --
 
 INSERT INTO `topers` (`id`, `img`, `name`, `course`, `semester`, `year`, `percentage`, `active`, `created_at`, `updated_at`) VALUES
-(3, '1725191136.png', 'Dhruvi', ' B.Com.', 'Sem-6', '2022', '95', 1, '2024-09-01 12:21:00', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `uNo` varchar(6) NOT NULL,
-  `nm` varchar(250) NOT NULL,
-  `eml` varchar(250) NOT NULL,
-  `mob` varchar(250) NOT NULL,
-  `addr` varchar(250) NOT NULL,
-  `dt` datetime NOT NULL,
-  `pwd` varchar(250) NOT NULL,
-  `profilePicture` text NOT NULL,
-  `v_code` varchar(250) NOT NULL,
-  `is_verify` varchar(250) NOT NULL,
-  `u_occupation` varchar(255) NOT NULL,
-  `u_profession` varchar(255) NOT NULL,
-  `u_city` varchar(255) NOT NULL,
-  `u_pincode` varchar(255) NOT NULL,
-  `u_birthdate` varchar(255) NOT NULL,
-  `u_about_fip` varchar(255) NOT NULL,
-  `u_instroctor_frd_name` varchar(255) NOT NULL,
-  `u_instroctor_phone` varchar(255) NOT NULL,
-  `is_member` varchar(250) NOT NULL DEFAULT 'No',
-  `u_enroll_course` varchar(250) NOT NULL DEFAULT '0' COMMENT 'numberof course user buy',
-  `u_enroll_course_ids` varchar(500) NOT NULL COMMENT 'enroll courses ids',
-  `is_volunteer` int(11) NOT NULL,
-  `updAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `uNo`, `nm`, `eml`, `mob`, `addr`, `dt`, `pwd`, `profilePicture`, `v_code`, `is_verify`, `u_occupation`, `u_profession`, `u_city`, `u_pincode`, `u_birthdate`, `u_about_fip`, `u_instroctor_frd_name`, `u_instroctor_phone`, `is_member`, `u_enroll_course`, `u_enroll_course_ids`, `is_volunteer`, `updAt`) VALUES
-(58, 'HM100X', 'xuponetydi', 'test@gmail.com', '9865321475', 'Libero expedita hic ', '2024-04-29 17:16:17', '$2y$10$Yq971OrjYOStFZyBUXua3.h3A60HsyiXL3mTyqjTHEWCu81D4sxEO', '1714391177.png', '946133', 'true', '', '', '', '', '', '', '', '', 'No', '0', '', 0, '2024-04-29 18:02:50'),
-(59, 'HF745V', 'test', 'test@gmail.com', '7016922901', 'Test', '2024-04-30 10:48:54', '$2y$10$16a2JhEe7QSogF85THNOcekwW3E0pfo9NskwCeUvkX.V.D00WgQKK', '', '', '', '', '', '', '', '', '', '', '', 'No', '0', '', 0, '0000-00-00 00:00:00'),
-(60, 'TS400I', 'test', 'test@gmail.com', '7016922901', 'Test', '2024-04-30 10:49:07', '$2y$10$5EDhNcjJrB4EFSMLo4cKsu9U/EYnuX2oFX59ZBykaXTO8vX1WcuoG', '1714454347.png', '', '', '', '', '', '', '', '', '', '', 'No', '0', '', 0, '0000-00-00 00:00:00'),
-(61, 'CP663G', 'test new', 'testnew@gmail.com', '7016922945', 'test address', '2024-04-30 10:51:37', '$2y$10$Ob/2a5qAusKOckRHiR7URO0IxN72PQQT3v5rXbK7z1hlrgikBwATK', '1714455357.png', '', '', '', '', '', '', '', '', '', '', 'No', '0', '', 0, '2024-04-30 11:05:57'),
-(62, 'JO705X', 'Dhruvi Sangani', 'dsangani44@gmail.com', '7016922908', 'Junagadh', '2024-05-13 11:02:13', '$2y$10$WVQJFZJk5L/TmJEvxVAx6.0Wd2qMQaFFqkbc7zZ92jclKiIBEchqi', '1715578333.png', '953174', 'true', '', '', '', '', '', '', '', '', 'No', '0', '', 0, '0000-00-00 00:00:00');
+(3, '1726936524.png', 'Radhika', ' B.Com.', 'Sem-6', '2022', '95', 1, '2024-09-21 16:35:44', '0000-00-00 00:00:00'),
+(4, '1726936532.png', 'Leo Villarreal', 'B.C.A.', 'Sem-5', '2005', '98', 1, '2024-09-21 16:35:32', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -641,12 +426,6 @@ ALTER TABLE `faq`
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `notificationhistory`
---
-ALTER TABLE `notificationhistory`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `testimonial`
@@ -713,12 +492,6 @@ ALTER TABLE `login`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=353;
 
 --
--- AUTO_INCREMENT for table `notificationhistory`
---
-ALTER TABLE `notificationhistory`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
-
---
 -- AUTO_INCREMENT for table `testimonial`
 --
 ALTER TABLE `testimonial`
@@ -728,7 +501,7 @@ ALTER TABLE `testimonial`
 -- AUTO_INCREMENT for table `topers`
 --
 ALTER TABLE `topers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
